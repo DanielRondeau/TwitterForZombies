460db5d87654f5685ca0a169364e9eb115e71469
Daniel Rondeau <dfr355@students.jwu.edu>
2014-08-04 20:31:21 -0400

    editing

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
- # encoding: UTF-8
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730200956) do
+ActiveRecord::Schema.define(version: 20140804003010) do
+
+  create_table "brains", force: true do |t|
+    t.integer  "zombie_id"
+    t.string   "status"
+    t.string   "flavor"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "zombies", force: true do |t|
     t.string   "name"
